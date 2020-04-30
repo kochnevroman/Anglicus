a598d5581e270933c1052687731d6ad784884812
Roman <33309887+kochnevroman@users.noreply.github.com>
2020-04-30 12:36:44 +0300

    Delete wordsSelection.py

--- wordsSelection.py
+++
@@ -1,5 +0,0 @@
-# Принимает на вход уровень слова, возвращяет все слова этого уровня
-
-def words_selection(lvl):
-    cursor.execute('select * from [Dictionary] where (difficulty_lvl = ?)', lvl)
-    return cursor.fetchall()
